d974b9866aee9c0c6527f38c9a1dc34569749610
mauriciovander <mauriciovander@gmail.com>
2014-07-10 19:46:12 -0300

    add "register" method change get and post methods to private

--- config/config.php
+++ config/config.php
@@ -1,5 +1,5 @@
 <?php
 
 define("GATEWAY_SERVER_USER", 'admin@user');
-define("GATEWAY_SERVER_PASS", 'very_secure_password');
-define("GATEWAY_SERVER_IP", 'IP.ADD.RE.SS');
+define("GATEWAY_SERVER_PASSWORD", 'very_secure_password');
+define("GATEWAY_SERVER_ADDRESS", 'IP.ADD.RE.SS');
--- lib/gateway.php
+++ lib/gateway.php
@@ -7,10 +7,15 @@
 class gateway {
 	// TODO - Insert your code here
 	private $gateway_url;
+	private $gateway_user;
+	private $gateway_password;
+	
 	/**
 	 */
 	function __construct() {
-		$this->gateway_url = 'https://' . GATEWAY_SERVER_IP;
+		$this->gateway_url = 'https://' . GATEWAY_SERVER_ADDRESS;
+		$this->gateway_user = GATEWAY_SERVER_USER;
+		$this->gateway_password = GATEWAY_SERVER_PASSWORD;
 	}
 	
 	/**
@@ -20,7 +25,38 @@ class gateway {
 		// TODO - Insert your code here
 	}
 	
-	public function post($method, $data = array()) {
+	
+	/**
+	 * This method is used for registering a user in the gateway
+	 * @param unknown $email
+	 * @param unknown $ripple_address
+	 * @return Ambigous <boolean, mixed>|boolean
+	 */
+	function register($email,$ripple_address){
+		
+		// validate ripple address:
+		$result = $this->get('/v1/trustlines/'.$ripple_address);
+		if($result && $result->success){
+			
+			// generate a Unique ID
+			$uid = uniqid();
+			
+			// register user in the gateway
+			$data = array(
+					'name'=>$email,
+					'password'=>$uid,
+					'ripple_address'=>$ripple_address,
+			);
+			$result = $this->post('/v1/registrations',$data);
+			if($result && $result->success) return $result;
+		}
+		return false;		
+	}
+	
+	
+	
+	
+	private function post($method, $data = array()) {
 		$url = $this->gateway_url . $method;
 		$encoded = http_build_query ( $data ); // substr($encoded, 0, strlen($encoded)-1);
 		
@@ -30,7 +66,7 @@ class gateway {
 		curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, 1 );
 		curl_setopt ( $ch, CURLOPT_FAILONERROR, 0 );
 		curl_setopt ( $ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC );
-		curl_setopt ( $ch, CURLOPT_USERPWD, RIPPLE_SERVER_USER . ":" . RIPPLE_SERVER_PASS );
+		curl_setopt ( $ch, CURLOPT_USERPWD, $this->gateway_user . ":" . $this->gateway_password );
 		curl_setopt ( $ch, CURLOPT_POST, 1 );
 		curl_setopt ( $ch, CURLOPT_HEADER, 0 ); // +
 		curl_setopt ( $ch, CURLOPT_POSTFIELDS, $encoded );
@@ -41,12 +77,16 @@ class gateway {
 		// curl_setopt($ch, CURLOPT_SSLCERT, $cert); // TODO: fix this so I can trust the SSL certificate
 		// curl_setopt($ch, CURLOPT_SSLCERTTYPE, 'PEM');
 		
-		$output = trim ( curl_exec ( $ch ) );
+		$json = trim ( curl_exec ( $ch ) );
 		curl_close ( $ch );		
-		return $output;
+		
+		$obj = json_decode($json);
+		if(json_last_error()==JSON_ERROR_NONE) return $obj;
+		return false;
+		
 	}
 	
-	public function get($method, $data = array()) {
+	private function get($method, $data = array()) {
 		$url = $this->gateway_url.$method;
 		if (count ( $data )) $url .= '?' . http_build_query ( $data );
 				
@@ -57,7 +97,7 @@ class gateway {
 		curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, 1 );
 		curl_setopt ( $ch, CURLOPT_FAILONERROR, 0 );
 		curl_setopt ( $ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC );
-		curl_setopt ( $ch, CURLOPT_USERPWD, RIPPLE_SERVER_USER . ":" . RIPPLE_SERVER_PASS );
+		curl_setopt ( $ch, CURLOPT_USERPWD, $this->gateway_user . ":" . $this->gateway_password );
 		curl_setopt ( $ch, CURLOPT_HEADER, 0 );
 		
 		curl_setopt ( $ch, CURLOPT_SSL_VERIFYPEER, 0 );
@@ -66,9 +106,12 @@ class gateway {
 		// curl_setopt($ch, CURLOPT_SSLCERT, $cert); // TODO: fix this so I can trust the SSL certificate
 		// curl_setopt($ch, CURLOPT_SSLCERTTYPE, 'PEM');
 		
-		$output = trim ( curl_exec ( $ch ) );
+		$json = trim ( curl_exec ( $ch ) );
 		curl_close ( $ch );
-		return $output;
+		
+		$obj = json_decode($json);
+		if(json_last_error()==JSON_ERROR_NONE) return $obj;
+		return false;
 	}
 }
 
