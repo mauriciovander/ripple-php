c567dce37305d097729e0cb30e762f0bd322cfd6
mauriciovander <mauriciovander@gmail.com>
2014-07-10 19:09:56 -0300

    generating basic structure

---
+++ config/config.php
@@ -0,0 +1,5 @@
+<?php
+
+define("GATEWAY_SERVER_USER", 'admin@user');
+define("GATEWAY_SERVER_PASS", 'very_secure_password');
+define("GATEWAY_SERVER_IP", 'IP.ADD.RE.SS');
---
+++ lib/gateway.php
@@ -0,0 +1,25 @@
+<?php
+
+/** 
+ * @author mauriciovander
+ *
+ */
+class gateway {
+	// TODO - Insert your code here
+	
+	/**
+	 */
+	function __construct() {
+		
+		// TODO - Insert your code here
+	}
+	
+	/**
+	 */
+	function __destruct() {
+		
+		// TODO - Insert your code here
+	}
+}
+
+?>
